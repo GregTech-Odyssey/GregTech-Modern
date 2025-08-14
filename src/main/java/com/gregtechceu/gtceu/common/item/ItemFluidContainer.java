6bb8d25bd113b6ca5ab973a0343146d565a95703
Wanshiheng <emotionalW@outlook.com>
2025-08-15 02:59:52 +0800

    修复刷单元问题

--- src/main/java/com/gregtechceu/gtceu/common/item/ItemFluidContainer.java
+++ src/main/java/com/gregtechceu/gtceu/common/item/ItemFluidContainer.java
@@ -6,15 +6,20 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fluids.FluidType;
 import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.fluids.capability.IFluidHandler.FluidAction;
+import net.minecraftforge.fluids.capability.templates.FluidHandlerItemStack;
 
 public class ItemFluidContainer implements IRecipeRemainder {
 
     @Override
     public ItemStack getRecipeRemained(ItemStack itemStack) {
         return FluidUtil.getFluidHandler(itemStack).map(handler -> {
-            var drained = handler.drain(FluidType.BUCKET_VOLUME, FluidAction.SIMULATE);
-            if (drained.getAmount() != FluidType.BUCKET_VOLUME) return ItemStack.EMPTY;
-            handler.drain(FluidType.BUCKET_VOLUME, FluidAction.EXECUTE);
+            int contained = FluidType.BUCKET_VOLUME;
+            if (handler instanceof FluidHandlerItemStack fluidHandler) {
+                contained = Math.max(contained, fluidHandler.getFluid().getAmount());
+            }
+            var drained = handler.drain(contained, FluidAction.SIMULATE);
+            if (drained.getAmount() != contained) return ItemStack.EMPTY;
+            handler.drain(contained, FluidAction.EXECUTE);
             return handler.getContainer();
         }).orElse(itemStack);
     }
