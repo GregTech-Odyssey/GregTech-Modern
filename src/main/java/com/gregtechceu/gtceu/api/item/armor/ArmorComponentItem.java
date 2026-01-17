58f220f9820951590726ca9bb719db630b7dc27b
remakefactory <215389873+remakefactory@users.noreply.github.com>
2026-01-17 14:00:38 +0800

    fix Same uuid

--- src/main/java/com/gregtechceu/gtceu/api/item/armor/ArmorComponentItem.java
+++ src/main/java/com/gregtechceu/gtceu/api/item/armor/ArmorComponentItem.java
@@ -65,7 +65,6 @@ public class ArmorComponentItem extends ArmorItem implements IComponentItem {
     public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
         Multimap<Attribute, AttributeModifier> multimap = ArrayListMultimap.create();
         IArmorLogic armorLogic = getArmorLogic();
-        multimap.putAll(super.getAttributeModifiers(slot, stack));
         multimap.putAll(armorLogic.getAttributeModifiers(slot, stack));
         return multimap;
     }
