f2b3fbd2525cd9e283d1c747c492394849427259
nutant <65939836+nutant233@users.noreply.github.com>
2026-05-26 13:42:41 +0800

    fix

--- src/main/java/com/gregtechceu/gtceu/api/recipe/handler/ICustomRecipeLogicHolder.java
+++ src/main/java/com/gregtechceu/gtceu/api/recipe/handler/ICustomRecipeLogicHolder.java
@@ -15,22 +15,28 @@ public interface ICustomRecipeLogicHolder extends IRecipeHandlerHolder {
 
     @Override
     default boolean findRecipe(GTRecipeType type, BiPredicate<RecipeHandlerUnit, GTRecipeDefinition> canHandle) {
-        var customRecipeLogic = type.getCustomRecipeLogicRunners();
-        var hasCustomRecipeLogic = !customRecipeLogic.isEmpty();
-        var searchRecipe = searchRecipe();
-        for (var unit : this.getInputUnits()) {
-            var r = this.createCustomRecipe(unit);
-            if (r != null && canHandle.test(unit, r)) return true;
-            if (searchRecipe) {
-                if (unit.findRecipe(type, canHandle)) return true;
-                if (hasCustomRecipeLogic) {
-                    for (var logic : customRecipeLogic) {
-                        r = logic.createCustomRecipe(this, unit);
-                        if (r != null && canHandle.test(unit, r)) return true;
+        var inputs = this.getInputUnits();
+        if (inputs.isEmpty()) {
+            var r = this.createCustomRecipe(RecipeHandlerUnit.NO_DATA);
+            return r != null && canHandle.test(RecipeHandlerUnit.NO_DATA, r);
+        } else {
+            var customRecipeLogic = type.getCustomRecipeLogicRunners();
+            var hasCustomRecipeLogic = !customRecipeLogic.isEmpty();
+            var searchRecipe = searchRecipe();
+            for (var unit : inputs) {
+                var r = this.createCustomRecipe(unit);
+                if (r != null && canHandle.test(unit, r)) return true;
+                if (searchRecipe) {
+                    if (unit.findRecipe(type, canHandle)) return true;
+                    if (hasCustomRecipeLogic) {
+                        for (var logic : customRecipeLogic) {
+                            r = logic.createCustomRecipe(this, unit);
+                            if (r != null && canHandle.test(unit, r)) return true;
+                        }
                     }
                 }
             }
+            return false;
         }
-        return false;
     }
 }
