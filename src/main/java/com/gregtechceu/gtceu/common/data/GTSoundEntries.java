2a8af155d240b2fe5fd3f2967ebb5c1faa1bc011
alcox233 <113848416+alcox233@users.noreply.github.com>
2026-09-16 09:02:29 +0800

    合并拉取请求 #47
    * feat: 新增无尽喷漆罐并完善交互
    
    * chore: 移除混入的机器内存卡 lang 条目
    
    * Merge remote-tracking branch 'gto/gto' into feature/infinite-spray-can
    
    * refactor: simplify CPacketSprayCanAction constructor and remove unuse…

--- src/main/java/com/gregtechceu/gtceu/common/data/GTSoundEntries.java
+++ src/main/java/com/gregtechceu/gtceu/common/data/GTSoundEntries.java
@@ -49,6 +49,8 @@ public class GTSoundEntries {
     public static final SoundEntry CHAINSAW_TOOL = REGISTRATE.sound("chainsaw").build();
     public static final SoundEntry WIRECUTTER_TOOL = REGISTRATE.sound("wirecutter").build();
     public static final SoundEntry SPRAY_CAN_TOOL = REGISTRATE.sound("spray_can").build();
+    /** GT5 infinite spray can color-switch “shake” sound. */
+    public static final SoundEntry SPRAY_CAN_SHAKE = REGISTRATE.sound("spraycan_shake").build();
     public static final SoundEntry PORTABLE_SCANNER = REGISTRATE.sound("portable_scanner").build();
     public static final SoundEntry MORTAR_TOOL = REGISTRATE.sound("mortar").build();
     public static final SoundEntry SUS_RECORD = REGISTRATE.sound("sus").build();
