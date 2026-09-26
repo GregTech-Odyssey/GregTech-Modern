1df879c10bc9473730672898c0aec71f273e798d
xinxinsuried <76897393+xinxinsuried@users.noreply.github.com>
2026-09-26 13:40:47 +0800

    fix: 机器开关按钮改为服务端同步显示、点击只在服务端修改

--- src/main/java/com/gregtechceu/gtceu/api/gui/fancy/IFancyConfiguratorButton.java
+++ src/main/java/com/gregtechceu/gtceu/api/gui/fancy/IFancyConfiguratorButton.java
@@ -1,10 +1,12 @@
 package com.gregtechceu.gtceu.api.gui.fancy;
 
+import com.gregtechceu.gtceu.uipro.data.SyncValue;
+import com.gregtechceu.gtceu.uipro.data.SyncValueHost;
+
 import com.lowdragmc.lowdraglib.gui.texture.IGuiTexture;
 import com.lowdragmc.lowdraglib.gui.util.ClickData;
 import com.lowdragmc.lowdraglib.gui.widget.Widget;
 
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 
@@ -15,7 +17,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.BooleanSupplier;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 public interface IFancyConfiguratorButton extends IFancyConfigurator {
@@ -54,7 +55,7 @@ public interface IFancyConfiguratorButton extends IFancyConfigurator {
         IGuiTexture pressed;
         BiConsumer<ClickData, Boolean> onClick;
         BooleanSupplier booleanSupplier;
-        boolean isPressed;
+        private final SyncValue<Boolean> state;
         Function<Boolean, List<Component>> tooltipsSupplier = isPressed -> Collections.emptyList();
 
         public Toggle(IGuiTexture base, IGuiTexture pressed, BooleanSupplier booleanSupplier, BiConsumer<ClickData, Boolean> onClick) {
@@ -62,48 +63,35 @@ public interface IFancyConfiguratorButton extends IFancyConfigurator {
             this.pressed = pressed;
             this.booleanSupplier = booleanSupplier;
             this.onClick = onClick;
+            this.state = SyncValue.of(booleanSupplier::getAsBoolean, SyncValue.BOOLEAN, false);
+        }
+
+        @Override
+        public void bindSync(SyncValueHost host) {
+            host.add(state);
+        }
+
+        public boolean isPressed() {
+            return state.getValue();
         }
 
         @Override
         public List<Component> getTooltips() {
-            return tooltipsSupplier.apply(isPressed);
-        }
-
-        @Override
-        public void detectAndSendChange(BiConsumer<Integer, Consumer<FriendlyByteBuf>> sender) {
-            var newIsPressed = booleanSupplier.getAsBoolean();
-            if (newIsPressed != isPressed) {
-                isPressed = newIsPressed;
-                sender.accept(0, buf -> buf.writeBoolean(isPressed));
-            }
-        }
-
-        @Override
-        public void readUpdateInfo(int id, FriendlyByteBuf buf) {
-            if (id == 0) {
-                isPressed = buf.readBoolean();
-            }
-        }
-
-        @Override
-        public void writeInitialData(FriendlyByteBuf buffer) {
-            this.isPressed = booleanSupplier.getAsBoolean();
-            buffer.writeBoolean(this.isPressed);
-        }
-
-        @Override
-        public void readInitialData(FriendlyByteBuf buffer) {
-            this.isPressed = buffer.readBoolean();
+            return tooltipsSupplier.apply(isPressed());
         }
 
         @Override
         public IGuiTexture getIcon() {
-            return isPressed ? pressed : base;
+            return isPressed() ? pressed : base;
         }
 
         @Override
         public void onClick(ClickData clickData) {
-            onClick.accept(clickData, !isPressed);
+            if (clickData.isRemote) {
+                if (!isPersistent()) onClick.accept(clickData, !isPressed());
+                return;
+            }
+            onClick.accept(clickData, !booleanSupplier.getAsBoolean());
         }
 
         @Override
@@ -113,7 +101,7 @@ public interface IFancyConfiguratorButton extends IFancyConfigurator {
 
         @Override
         public boolean isLatched() {
-            return isPressed && isPersistent();
+            return isPressed() && isPersistent();
         }
 
         /**
